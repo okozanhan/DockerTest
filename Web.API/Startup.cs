f94f2d7591bfe758fba6748c6d8011f5d05e5d0d
okozanhan <okozanhan01@gmail.com>
2021-11-04 14:09:28 +0300

    ASDASD

--- Web.API/Startup.cs
+++ Web.API/Startup.cs
@@ -37,7 +37,7 @@ namespace Web.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
+          
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web.API v1"));
